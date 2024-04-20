82acc88c157a87d101c64735f8731213a1ab7dfe
Gromozeka-BI <boyarinov.ivan081202@yandex.ru>
2024-04-20 16:27:07 +0300

    BB v1.1

--- src/main/blackbox/blackbox.c
+++ src/main/blackbox/blackbox.c
@@ -807,21 +807,21 @@ static void writeInterframe(void)
     DEBUG_SET(DEBUG_BIG_BLACK, 2, lrintf(blackboxCurrent->setpoint[2])); // угловая скорость (YAW) работает
     //DEBUG_SET(DEBUG_BIG_BLACK, 3, lrintf(blackboxCurrent->setpoint[3])); // тут надо поправить данные работает
     //DEBUG_SET(DEBUG_BIG_BLACK, 3, lrintf(mixerGetThrottle() * 1000)); // попробовать так писать газ
-    DEBUG_SET(DEBUG_BIG_BLACK, 7, lrintf(gpsSol.numSat));
-    DEBUG_SET(DEBUG_BIG_BLACK, 8, lrintf(gpsSol.llh.altCm / 10));
-    DEBUG_SET(DEBUG_BIG_BLACK, 9, lrintf(gpsSol.groundSpeed));
-    DEBUG_SET(DEBUG_BIG_BLACK, 10, lrintf(gpsSol.groundCourse));
-    DEBUG_SET(DEBUG_BIG_BLACK, 11, gpsSol.llh.lon);
-    DEBUG_SET(DEBUG_BIG_BLACK, 12, gpsSol.llh.lat);
+    //DEBUG_SET(DEBUG_BIG_BLACK, 7, lrintf(gpsSol.numSat));
+    //DEBUG_SET(DEBUG_BIG_BLACK, 8, lrintf(gpsSol.llh.altCm / 10));
+    //DEBUG_SET(DEBUG_BIG_BLACK, 9, lrintf(gpsSol.groundSpeed));
+    //DEBUG_SET(DEBUG_BIG_BLACK, 10, lrintf(gpsSol.groundCourse));
+    //DEBUG_SET(DEBUG_BIG_BLACK, 11, gpsSol.llh.lon);
+    //DEBUG_SET(DEBUG_BIG_BLACK, 12, gpsSol.llh.lat);
 
-    const int degreesPartLon = labs(gpsSol.llh.lon) / GPS_DEGREES_DIVIDER;
-    int fractionalPartLon = labs(gpsSol.llh.lon) % GPS_DEGREES_DIVIDER;
-    const int degreesPartLat = labs(gpsSol.llh.lat) / GPS_DEGREES_DIVIDER;
-    int fractionalPartLat = labs(gpsSol.llh.lat) % GPS_DEGREES_DIVIDER;
-    DEBUG_SET(DEBUG_BIG_BLACK, 13, lrintf(degreesPartLon));
-    DEBUG_SET(DEBUG_BIG_BLACK, 14, lrintf(fractionalPartLon));
-    DEBUG_SET(DEBUG_BIG_BLACK, 15, lrintf(degreesPartLat));
-    DEBUG_SET(DEBUG_BIG_BLACK, 16, lrintf(fractionalPartLat));
+    //const int degreesPartLon = labs(gpsSol.llh.lon) / GPS_DEGREES_DIVIDER;
+    //int fractionalPartLon = labs(gpsSol.llh.lon) % GPS_DEGREES_DIVIDER;
+    //const int degreesPartLat = labs(gpsSol.llh.lat) / GPS_DEGREES_DIVIDER;
+    //int fractionalPartLat = labs(gpsSol.llh.lat) % GPS_DEGREES_DIVIDER;
+    //DEBUG_SET(DEBUG_BIG_BLACK, 13, lrintf(degreesPartLon));
+    //DEBUG_SET(DEBUG_BIG_BLACK, 14, lrintf(fractionalPartLon));
+    //DEBUG_SET(DEBUG_BIG_BLACK, 15, lrintf(degreesPartLat));
+    //DEBUG_SET(DEBUG_BIG_BLACK, 16, lrintf(fractionalPartLat));
 //BIG_BLACK    
 
     if (testBlackboxCondition(CONDITION(RC_COMMANDS))) {
